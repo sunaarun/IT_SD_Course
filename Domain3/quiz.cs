ab7e4b1c2bf92bc34073cc424e77d84756ae1291
sunaarun <sanaharun91@gmail.com>
2023-11-24 22:24:39 +0100

    IT_Course_With_Quizez

--- Domain3/quiz.cs
+++ Domain3/quiz.cs
@@ -42,13 +42,3 @@ public class D_child:C_child
         base.AddNumbers(a,b);
     }
 }
-/*
- A_Parent a = new A_Parent();
-        B_child b = new B_child();
-        C_child c = new C_child();
-        D_child d = new D_child();
-        a.DisplayMessage();
-        b.DisplayMessage();
-        c.DisplayMessage();
-        d.Sum(3, 6);
-*/
---
+++ S/oopS.cs
@@ -0,0 +1,10 @@
+/*
+ A_Parent a = new A_Parent();
+        B_child b = new B_child();
+        C_child c = new C_child();
+        D_child d = new D_child();
+        a.DisplayMessage();
+        b.DisplayMessage();
+        c.DisplayMessage();
+        d.Sum(3, 6);
+*/
