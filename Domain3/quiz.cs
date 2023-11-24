6c2fa93a97e5311adc0bb4696c4ac81c75e40fcc
sunaarun <sanaharun91@gmail.com>
2023-11-24 22:18:41 +0100

    IT_Course With Quizez

---
+++ Domain3/quiz.cs
@@ -0,0 +1,54 @@
+using System;
+
+// Base class
+public class A_Parent
+{
+    public virtual void DisplayMessage()
+    {
+        Console.WriteLine("from A");
+    }
+    public void Sum(int a,int b)
+    {
+       int c = a+b;
+        Console.WriteLine("Inside  A, the sum is "+c );
+    }
+}
+
+// Derived class 1
+public class B_child : A_Parent
+{
+    public override void DisplayMessage()
+    {
+        Console.WriteLine("Inside B");
+    }
+}
+
+// Derived class 2
+public class C_child : A_Parent
+{
+    public override void DisplayMessage()
+    {
+        Console.WriteLine("Inside C");
+    }
+    public void AddNumbers(int a, int b)
+    {
+        base.Sum(a,b);
+    }
+}
+public class D_child:C_child
+{
+    public void SumC(int a, int b)
+    {
+        base.AddNumbers(a,b);
+    }
+}
+/*
+ A_Parent a = new A_Parent();
+        B_child b = new B_child();
+        C_child c = new C_child();
+        D_child d = new D_child();
+        a.DisplayMessage();
+        b.DisplayMessage();
+        c.DisplayMessage();
+        d.Sum(3, 6);
+*/
--- Program.cs
+++ Program.cs
@@ -6,61 +6,96 @@ using DelegateExample;
 class IT
 {
     static void Main(string[] args)
-    {  
-      MVCPatternDemo mVCPattern = new MVCPatternDemo();
-       //fetch student record based on his roll no from the database
-      Student model  = mVCPattern.retriveStudentFromDatabase();
+    {
+        /* for (;;)
+        {
+            // Code inside the loop
+            Console.WriteLine("hello");
+        }*/
+       // int[] arr = { 1, 5, 4, 3, 0 };
+       // SortClass sort = new SortClass();
+     //   sort.mergeSorting(arr);
+            Stack<int> myStack = new Stack<int>();
+        Queue<int> queue1 = new Queue<int>();
+        Queue<int> queue2 = new Queue<int>();
 
-      //Create a view : to write student details on console
-      StudentView view = new StudentView();
+        myStack.Push(10);
+        myStack.Push(20);
+        myStack.Push(30);
 
-      StudentController controller = new StudentController(model, view);
+        queue1.Enqueue(40);
+        queue1.Enqueue(50);
+        queue1.Enqueue(60);
+        queue1.Enqueue(myStack.Pop());
 
-      controller.updateView();
 
-      //update model data
-      controller.setStudentName("John");
+        queue2.Enqueue(queue1.Dequeue());
+        queue2.Enqueue(queue1.Dequeue());
+        queue2.Enqueue(myStack.Pop());
+        queue2.Enqueue(myStack.Pop());
+        foreach(int q in queue2)
+        {
+            Console.WriteLine(q);
 
-      controller.updateView();
-     Console.WriteLine("Sanaa");
+        }
     }
-   
+
 }
 
 
 /*
 You can use these here inside Main function, 
 */
-     // Console.WriteLine("Enter the number of Fibonacci numbers to print: ");
-        // int count = int.Parse(Console.ReadLine());
-        //  Fibonacci fibonacci =new Fibonacci();
-        // Console.WriteLine("Fibonacci Sequence:");
-        // fibonacci.PrintFibonacciSequence(count);
-        //LogicalError logical = new LogicalError();
-        //logical.logic(5,6);
+// Console.WriteLine("Enter the number of Fibonacci numbers to print: ");
+// int count = int.Parse(Console.ReadLine());
+//  Fibonacci fibonacci =new Fibonacci();
+// Console.WriteLine("Fibonacci Sequence:");
+// fibonacci.PrintFibonacciSequence(count);
+//LogicalError logical = new LogicalError();
+//logical.logic(5,6);
 
- /* ----------------------------------------------------------*/
-     //int [] arr = {1,5,4,3,0};
-        //MyEvent myEvent = new MyEvent();
-        //MyDelegate myDelegate= myEvent.DisplayMessage;
-        //myDelegate("Sanaa"); 
-       // SortClass sort =  new SortClass();
-        //sort.selection(arr);
-        
-  /*--------------------------------------------------------------*/
-   //StaticExample.name="Sanaa";
-    //   // StaticExample.DisplayMessage();
+/* ----------------------------------------------------------*/
+//int [] arr = {1,5,4,3,0};
+//MyEvent myEvent = new MyEvent();
+//MyDelegate myDelegate= myEvent.DisplayMessage;
+//myDelegate("Sanaa"); 
+// SortClass sort =  new SortClass();
+//sort.selection(arr);
 
-    //     Car c1=new Car("Red", 250, 2020, "2020- car");
-    //     c1.car_message(); // 
-    //     c1.stop();
-    //     //Console.WriteLine(c1.color + " "+ c1.model);
-    //     // Vehicle veh1 = new Vehicle();
-    //     // veh1.model=2019;
-    //     // veh1.name="Vehicle 2019";
-    //     // Console.WriteLine("Name: "+ veh1.name + "  Model: " +veh1.model +"\n");
-    //     // // Vehicle v2= new Vehicle();
-    //     // // v2.model=2001;
-    //     // // v2.name="Vehicle 2001";
-    //     // //  Console.Write("Name: "+ v2.name + " is " );
-    //     // // v2.move(); // calling move method      
+/*--------------------------------------------------------------*/
+//StaticExample.name="Sanaa";
+//   // StaticExample.DisplayMessage();
+
+//     Car c1=new Car("Red", 250, 2020, "2020- car");
+//     c1.car_message(); // 
+//     c1.stop();
+//     //Console.WriteLine(c1.color + " "+ c1.model);
+//     // Vehicle veh1 = new Vehicle();
+//     // veh1.model=2019;
+//     // veh1.name="Vehicle 2019";
+//     // Console.WriteLine("Name: "+ veh1.name + "  Model: " +veh1.model +"\n");
+//     // // Vehicle v2= new Vehicle();
+//     // // v2.model=2001;
+//     // // v2.name="Vehicle 2001";
+//     // //  Console.Write("Name: "+ v2.name + " is " );
+//     // // v2.move(); // calling move method      
+
+
+/**
+MVCPatternDemo mVCPattern = new MVCPatternDemo();
+        //fetch student record based on his roll no from the database
+        Student model = mVCPattern.retriveStudentFromDatabase();
+
+        //Create a view : to write student details on console
+        StudentView view = new StudentView();
+
+        StudentController controller = new StudentController(model, view);
+
+        controller.updateView();
+
+        //update model data
+        controller.setStudentName("John");
+
+        controller.updateView();
+        Console.WriteLine("Sanaa");
+       */
